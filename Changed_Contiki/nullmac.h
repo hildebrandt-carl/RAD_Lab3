bc7250cf871a585c09ed7dee651af1bbfcdd2e42
Carl Hildebrandt <childebrandt@cse-elbaum-13.local>
2017-11-08 10:18:30 -0600

    Added the changed contiki code

---
+++ Changed_Contiki/nullmac.h
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2015, Swiss Federal Institute of Technology (ETH Zurich).
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author:  Federico Ferrari
+ *          Marco Zimmerling
+ */
+
+/**
+ * @addtogroup  Net
+ * @{
+ *
+ * @defgroup    nullmac Nullmac
+ * @{
+ *
+ * @file 
+ */
+ 
+#ifndef __NULLMAC_H__
+#define __NULLMAC_H__
+
+#include "platform.h"
+
+#ifndef NULLMAC_CONF_ON
+#define NULLMAC_CONF_ON         0
+#endif /* NULLMAC_CONF_ON */
+
+#if NULLMAC_CONF_ON
+
+typedef uint16_t addr_t;
+
+void nullmac_init(void);
+
+void unicast_send(void *payload, uint8_t payload_len, addr_t destination);
+void broadcast_send(void *payload, uint8_t payload_len);
+
+void unicast_received(rtimer_clock_t *timestamp, void *payload, 
+                             uint8_t payload_len, addr_t source);
+void broadcast_received(rtimer_clock_t *timestamp, void *payload, 
+                               uint8_t payload_len, addr_t source);
+
+void getReceivedMessage(uint8_t * str);
+
+#endif /* NULLMAC_CONF_ON */
+
+#endif /* __NULLMAC_H__ */
+
+/**
+ * @}
+ * @}
+ */
